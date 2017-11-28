644e8891fbbf132057291985d0f6fdafadf92c48
powdevel <luis.aveiga@gmail.com>
2017-11-28 13:10:36 -0500

    fix: fix typo on import

--- src/posts/reducer.js
+++ src/posts/reducer.js
@@ -1,7 +1,7 @@
 // importing action names
 import { POST_CREATE, POST_EDIT, POST_DELETE} from "./actions";
 // importing action creators
-import post_edit_action, post_delete_action, post_create_action } from "./actions";
+import { post_edit_action, post_delete_action, post_create_action } from "./actions";
 
 function reducer(state = {}, action) {
   return {};
