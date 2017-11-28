0be68a8b11a83900dabb3cd26577f57b87411c6a
powdevel <luis.aveiga@gmail.com>
2017-11-28 13:32:24 -0500

    Add thunk to store to allow asynchronicity

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { combineReducers } from "redux";
 import commentsReducer from "./comments/reducer";
 import postsReducer from "./posts/reducer";
@@ -6,7 +6,9 @@ import thunk from "redux-thunk";
 
 // Combining all reducers in the App
 const rootReducer = combineReducers({ commentsReducer, postsReducer });
+// Creating composed middleware
+const composedMiddleware = compose( applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
 // Creating the store and second argument for debugging purposes with Redux Devtools
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, composedMiddleware );
 
 export default store;
