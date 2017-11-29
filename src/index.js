ac37b82c9bb1f7122f3fb868a0892bceb55f0ee5
powdevel <luis.aveiga@gmail.com>
2017-11-29 16:01:19 -0500

    FIX: Remove import store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './general/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
 import store from './store';
 import { Provider } from 'react-redux';
 
