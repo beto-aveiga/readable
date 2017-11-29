47ad0f46c1b0085547ea5cac43148c5d4cf87e6e
powdevel <luis.aveiga@gmail.com>
2017-11-29 15:56:59 -0500

    Create Posts Reducer

--- src/posts/reducer.js
+++ src/posts/reducer.js
@@ -1,10 +1,18 @@
 // importing action names
-import { POST_CREATE, POST_EDIT, POST_DELETE} from "./actions";
+import { POST_CREATE, POST_EDIT, POST_DELETE, POSTS_INIT_ACTION } from "./actions";
 // importing action creators
 import { post_edit_action, post_delete_action, post_create_action } from "./actions";
 
 function reducer(state = {}, action) {
-  return {};
+  const { posts } = action;
+  switch (action.type) {
+    case POSTS_INIT_ACTION:
+      return Object.assign({}, state, {
+        posts: posts
+      });
+      default:
+      return state;
+  }
 }
 
 export default reducer;
