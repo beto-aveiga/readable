0f107805d74ff3df88a6afdb9e2c3277276a71a8
powdevel <luis.aveiga@gmail.com>
2017-11-29 15:59:32 -0500

    Create COMMENTS_INIT_ACTION

--- src/comments/actions.js
+++ src/comments/actions.js
@@ -1,6 +1,9 @@
+import server from "../general/readableServer";
+
 export const COMMENT_CREATE = 'COMMENT_CREATE'
 export const COMMENT_EDIT = 'COMMENT_EDIT'
 export const COMMENT_DELETE = 'COMMENT_DELETE'
+export const COMMENTS_INIT_ACTION = 'COMMENTS_INIT_ACTION'
 
 export const comment_create_action = comment => ({
     type: COMMENT_CREATE,
@@ -16,3 +19,13 @@ export const comment_edit_action = comment => ({
     type: COMMENT_EDIT,
     comment
 })
+
+
+export const comments_init_action = comments => ({
+  type: COMMENTS_INIT_ACTION,
+  comments
+});
+
+export const get_all_comments = (postId) => dispatch => {
+  server.getComments(postId).then(comments => dispatch(comments_init_action(comments)));
+};
