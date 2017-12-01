4c457dc423bd9cce4723dd58060e1b8a6b1c1374
powdevel <luis.aveiga@gmail.com>
2017-12-01 07:32:39 -0500

    Use _.unionBy to keep comments unique

--- src/comments/reducer.js
+++ src/comments/reducer.js
@@ -1,7 +1,17 @@
-// import * from './actions'
+import { COMMENTS_IN_POST_ACTION } from "./actions";
+import _ from "lodash";
 
-function reducer (state = {}, action) {
-    return {}
+function reducer(state = { comments: [] }, action) {
+  const { comments } = action;
+
+  switch (action.type) {
+    case COMMENTS_IN_POST_ACTION:
+      return Object.assign({}, state, {
+        comments:_.unionBy( comments, state.comments, 'id' )
+      });
+    default:
+      return state;
+  }
 }
 
-export default reducer
+export default reducer;
