ec87ae5e28eabd3726a261a86c8a578469481a2b
powdevel <luis.aveiga@gmail.com>
2017-12-01 13:27:39 -0500

    Implement upVote and downVote on post component

--- src/posts/post.js
+++ src/posts/post.js
@@ -1,25 +1,39 @@
 import React from "react";
-// import {get_comments} from "../comments/actions"
 import Comments from "../comments";
+import { post_upVote, post_downVote } from "./actions";
 import Rate from "../rate/rate";
 import { connect } from "react-redux";
+import _ from "lodash";
 
 class Post extends React.Component {
-
   componentDidMount() {
-      // debugger;
+    // debugger;
     // this.props.dispatch(get_comments( this.props.id ));
   }
 
+  upVote = () => {
+    this.props.dispatch(post_upVote(this.props.id));
+  };
+
+  downVote = () => {
+    this.props.dispatch(post_downVote(this.props.id));
+  };
+
   render() {
     return (
-        <div>POST {this.props.id}
-            <Rate like={ () => {console.log('LIKE')} } dislike={ ()=> {console.log('DISLIKE')}} voteScore={ this.props.voteScore } />
-            <Comments postId={this.props.id} comments={[]} />
-        </div>
+      <div>
+        POST {this.props.id}
+        <Rate like={this.upVote} dislike={this.downVote} voteScore={this.props.voteScore} />
+        <Comments postId={this.props.id} />
+      </div>
     );
   }
 }
 
+function mapStoreToProps(store, own_props) {
+  let posts = _.get(store, "postsReducer.posts", false);
+  let post = posts ? posts.filter(post => post.id === own_props.id) : [];
+  return post[0];
+}
 
-export default connect()(Post);
+export default connect(mapStoreToProps)(Post);
