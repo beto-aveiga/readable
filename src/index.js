7edf17cd4f6f014821c552206d39344cd13dd43f
powdevel <luis.aveiga@gmail.com>
2017-12-02 09:32:32 -0500

    Add tachyons for functional CSS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './general/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 import { Provider } from 'react-redux';
+import 'tachyons';
 
 // Providing store to everybody with provider
 ReactDOM.render( <Provider store={store}><App /></Provider>, document.getElementById('root'));
