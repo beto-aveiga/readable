50a45b9651933ff386b471409fe09e5a4bac9ae4
powdevel <luis.aveiga@gmail.com>
2018-01-12 11:40:28 -0500

    Introducing animations of posts (first try)

--- src/posts/index.js
+++ src/posts/index.js
@@ -3,28 +3,32 @@ import { connect } from "react-redux";
 import { get_all_posts } from "./actions";
 import Post from "./post";
 import _ from "lodash";
+import "anicollection/dist/anicollection.css";
 
 class Posts extends React.Component {
-
   componentDidMount() {
     this.props.dispatch(get_all_posts());
   }
 
   render() {
     return (
-        <div className="mw9 center sans-serif">
-            <div className="f2 b tc ma4">READABLE</div>
-            <div>
-                { this.props.posts.map(post => <Post key={post.id} {...post} />) }
-            </div>
-        </div>
+      <div>
+          {this.props.posts.map((post, i, arr) => {
+              post.votescore = post.voteScore;
+              post.commentcount = post.commentCount;
+              delete post.voteScore;
+              delete post.commentCount;
+              post.deleted = post.deleted.toString();
+              return <Post key={post.id} {...post} show={this.props.show} animdelin={`anim-del-0${i}0 anim-dur-050`} animdelout={`anim-del-0${arr.length - i}0 anim-dur-050`} />;
+          })}
+      </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-    let posts = _.get(state, 'postsReducer.posts', false);
-    return { posts: posts ? posts: [] }
+  let posts = _.get(state, "postsReducer.posts", false);
+  return { posts: posts ? posts : [] };
 }
 
 export default connect(mapStateToProps)(Posts);
