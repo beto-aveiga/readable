9e4f52b8146cf70d6c278ef53bb023c627a137fe
powdevel <luis.aveiga@gmail.com>
2018-01-12 14:40:21 -0500

    Change the category with link component

--- src/categories/list.js
+++ src/categories/list.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
 import server from "../general/readableServer";
+import { Link, Route, withRouter } from "react-router-dom";
 
 class Categories extends React.Component {
   state = {
@@ -20,9 +21,9 @@ class Categories extends React.Component {
 
   render() {
     return (
-      <div className="db tc ma2 mt4">
-          {this.state.names.map( (c,i) => <span key={c.path} className={`f6 ph3 pv2 mb2 dib white bg-red mh2 dim pointer animated flipInY anim-del-0${i}0 `}>{ c.name }</span>)}
-      </div>
+        <div className="db tc ma2 mt4">
+            {this.state.names.map( (c,i) => <Link key={c.path} className={`f6 ph3 pv2 mb2 dib white bg-red mh2 no-underline dim pointer animated flipInY anim-del-0${i}0 `} to={`/${c.name}`} >{ c.name }</Link>)}
+        </div>
     );
   }
 }
