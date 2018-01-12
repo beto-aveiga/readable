96f844d80b6a85d2edaa4c1e221ebb7fb1dfa957
powdevel <luis.aveiga@gmail.com>
2018-01-12 16:05:04 -0500

    REFACTOR set properties of post to lowercase

--- src/posts/reducer.js
+++ src/posts/reducer.js
@@ -4,27 +4,29 @@ import { POST_CREATE, POST_EDIT, POST_DELETE, POSTS_INIT_ACTION, POST_UPVOTE_ACT
 import { post_edit_action, post_delete_action, post_create_action } from "./actions";
 import _ from "lodash";
 
+// to make posts props to lowercase
+function postPropsToLower(e) {
+    e.commentcount = e.commentCount;
+    e.votescore = e.voteScore;
+    delete e.voteScore;
+    delete e.commentCount;
+    return e;
+}
+
 function reducer(state = {}, action) {
   switch (action.type) {
     case POSTS_INIT_ACTION: {
-      const { posts } = action;
-      // setting props to lowercase
-      const posts_propsTolower = posts.map( e => {
-          e.commentcount = e.commentCount;
-          e.votescore = e.voteScore;
-          delete e.voteScore;
-          delete e.commentCount;
-          return e;
-      });
-      return _.assign({}, state, { 'posts':posts_propsTolower });
+      let { posts } = action;
+      posts = posts.map(postPropsToLower);
+      return _.assign({}, state, { posts });
     }
 
     case POST_DOWNVOTE_ACTION:
     case POST_UPVOTE_ACTION: {
-      const { post } = action;
+      let { post } = action;
       const { posts } = state;
       const index_post_to_update = _.findIndex(state.posts, state_post => post.id === state_post.id);
-      posts[index_post_to_update] = post;
+      posts[index_post_to_update] = postPropsToLower(post);
       return _.assign({}, state, { posts });
     }
     default:
