51bab5b40df353621c6ce9324548180856c075e5
powdevel <luis.aveiga@gmail.com>
2018-01-14 11:55:57 -0500

    Remove connect from categories list because it does not need redux

--- src/categories/list.js
+++ src/categories/list.js
@@ -22,7 +22,7 @@ class Categories extends React.Component {
   render() {
     return (
         <div className="db tc ma2 mt4">
-            {this.state.names.map( (c,i) => <Link key={c.path} className={`f6 ph3 pv2 mb2 dib white bg-red mh2 no-underline dim pointer animated flipInY anim-del-0${i}0 `} to={`/${c.name}`} >{ c.name }</Link>)}
+            {this.state.names.map( (c,i) => <Link key={c.path} className={`f6 ph3 pv2 mb2 dib white bg-purple mh2 no-underline dim pointer animated flipInY anim-del-0${i}0 `} to={`/${c.name}`} >{ c.name }</Link>)}
         </div>
     );
   }
@@ -33,4 +33,4 @@ class Categories extends React.Component {
 //     return { posts: posts ? posts: [] }
 // }
 
-export default connect()(Categories);
+export default Categories;
