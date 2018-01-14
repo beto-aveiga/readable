386f1bd6c26479818c10e35d92c12a07e22ca0b6
powdevel <luis.aveiga@gmail.com>
2018-01-14 11:52:32 -0500

    REFACTOR and re-style of the comment counter

--- src/comments/comments_count.js
+++ src/comments/comments_count.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import React from "react";
-import _ from "lodash";
+// import _ from "lodash";
 
 
 class Comments_Count extends React.Component {
@@ -10,33 +10,19 @@ class Comments_Count extends React.Component {
     // this.props.dispatch(get_comments( this.props.id ));
   }
 
+  commentIcon = (
+      <i className="material-icons gray v-mid mr1 black--50">comment</i>
+  )
+
   render() {
-
-      switch (this.props.commentscount) {
-
-          case 0:
-            return (
-                <div className="">
-                    No comments
-                </div>
-            );
-        case 1:
-            return (
-                <div className="">
-                    1 comment
-                </div>
-            );
-        default:
-            return (
-                <div className="">
-                    {this.props.commentscount} comments
-                </div>
-            )
+      switch (this.props.commentcount) {
+        case 0: return (<div className="">{ this.commentIcon } No comments</div>);
+        case 1: return (<div className="">{ this.commentIcon } 1 comment</div>);
+        default: return (<div className="">{ this.commentIcon } {this.props.commentcount} comments</div>)
       }
-
   }
 }
 
 
 
-export default connect()(Comments_Count);
+export default Comments_Count;
