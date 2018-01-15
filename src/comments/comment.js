ef11d71edc521834ba34024eba6b1bebfc5787e2
powdevel <luis.aveiga@gmail.com>
2018-01-15 12:02:14 -0500

    Format presentation of single comment

--- src/comments/comment.js
+++ src/comments/comment.js
@@ -4,6 +4,7 @@ import Rate from "../rate/rate"
 import { COMMENT_UPVOTE_ACTION, COMMENT_DOWNVOTE_ACTION } from "./actions"
 import { comment_upVote, comment_downVote } from "./actions"
 import _ from "lodash";
+import { Link, Route, withRouter } from "react-router-dom";
 
 
 class Comment extends React.Component {
@@ -25,16 +26,37 @@ class Comment extends React.Component {
   render() {
     return (
 
-        <div className="pa0 tl">
-            <blockquote className="athelas ml0 mt0 pa2 mv3 black-90  b--black-40 ma0">
+        <div className="pa0 tl shadow-4 mb4">
+            <blockquote className="athelas ml0 mt0 pa mv3 black-90  b--black-40 ma0">
                 <p className="f5 lh-copy mt0 ">
-                    { this.props.body }
-                    <cite className="ml2" >―{this.props.author}</cite>
+                    <div className="bg-near-white pv4 ph4 gray tracked-mega sans-serif relative" >
+                        { this.props.body } <cite className="ml2" >―{this.props.author}</cite>
+                        <i className="material-icons f1 absolute right-1 top-1 white">mode_comment</i>
+                    </div>
+
                 </p>
 
-                <div>
-                    <Rate className="fl " like={this.upVote} dislike={this.downVote} votescore={this.props.voteScore} />
+                <div className="overflow-hidden sans-serif f6 ph4">
+
+
+                    <div className=" bg-white pv2 fr w-third tr">
+                        <Link to={`/comments/${this.props.id}/edit`} className="no-underline">
+                            <i className="material-icons v-mid mr1 f5 blue">mode_edit</i> <span className="ttu f6 black">Edit</span>
+                        </Link>
+                        <Link to={`/comments/${this.props.id}/delete`} className="no-underline">
+                            <i className="material-icons v-mid mr1 ml3 f5 red">delete</i> <span className="ttu f6 black">Delete</span>
+                        </Link>
+                    </div>
+
+                    <div className="fr w-third pa tc">
+                        <div className=" bg-white pv2">
+                            <Rate like={this.upVote} dislike={this.downVote} votescore={this.props.voteScore} />
+                        </div>
+                    </div>
+
+
                 </div>
+
             </blockquote>
         </div>
 
