1429d4a7aebdd0a480b6fc0812d55723002063e1
powdevel <luis.aveiga@gmail.com>
2018-01-17 14:05:53 -0500

    Read categories at the start of the app and store it in state

--- src/general/App.js
+++ src/general/App.js
@@ -3,22 +3,33 @@ import { connect } from "react-redux";
 import Posts from "../posts";
 import Post from "../posts/post";
 import Categories from "../categories/list";
+import server from "../general/readableServer";
 
 import { Link, Route, withRouter } from "react-router-dom";
 
-
 class App extends Component {
+  leavingComponents = 0;
 
-    leavingComponents = 0;
+  state = {
+    categories: []
+  };
 
-    f = {
-        '/categories': <Categories />,
-        '/posts': <Posts />
-    }
+  readCategories = () => {
+    server.getCategories().then(data => {
+      this.setState({ categories: data.categories });
+    });
+  };
 
+  f = {
+    "/categories": <Categories />,
+    "/posts": <Posts />
+  };
+
+  componentDidMount() {
+      this.readCategories();
+  }
 
   render() {
-
     // const path = this.props.location.pathname.split('/');
 
     return (
@@ -28,36 +39,41 @@ class App extends Component {
               <span className="dim dib animated fadeInRight anim-del-010 dark-red  anim-dur-020">able</span>
           </div>
 
-          <Link to="/" className="db f5 tc animated fadeIn delay-sm no-underline blue">home</Link>
+          <Link to="/" className="db f5 tc animated fadeIn delay-sm no-underline blue">
+              home
+          </Link>
 
+          <Categories categories={this.state.categories} />
 
-          <Categories />
-
-          <Route exact path="/" render= {() =>(
-              <div className="tc cb overflow-hidden mv5">
-                  <div className="f1 ph3  gray"><i className="material-icons f1">home</i></div>
-                  <div className="w50 bt b--silver tracked-mega ttu f6 pa3">all posts</div>
-              </div>
-
-
-          )} />
-
+          <Route
+              exact
+              path="/"
+              render={() => (
+                  <div className="tc cb overflow-hidden mv5">
+                      <div className="f1 ph3  gray">
+                          <i className="material-icons f1">home</i>
+                      </div>
+                      <div className="w50 bt b--silver tracked-mega ttu f6 pa3">all posts</div>
+                  </div>
+              )}
+          />
 
           <Route exact path="/" component={Posts} />
           <Route exact path="/posts/:category/:id" component={Post} />
-          <Route exact path="/posts/:category/:id/edit" component={Post} />
+          <Route exact path="/posts/:category/:id/edit" render={ () =><Post categories={this.state.categories} />  } />
 
           <Route exact path="/:category" component={Posts} />
 
-
-          <footer className=" bottom-0 left-0 pv4 ph3 ph5-m ph6-l mid-gray fl w-100 cf ">
-              <small className="f6 db tc">REACT & REDUX TOY PROJECT <b className="ttu mh2">READABLE</b> UDACITY NANODEGREE</small>
-              <div className="tc mt3">
-                  <a href="/privacy/"  title="Privacy" className="f6 dib ph2 link mid-gray dim">luis.aveiga@gmail.com</a>
-              </div>
-          </footer>
-
-
+        <footer className=" bottom-0 left-0 pv4 ph3 ph5-m ph6-l mid-gray fl w-100 cf ">
+          <small className="f6 db tc">
+            REACT & REDUX TOY PROJECT <b className="ttu mh2">READABLE</b> UDACITY NANODEGREE
+          </small>
+          <div className="tc mt3">
+            <a href="/privacy/" title="Privacy" className="f6 dib ph2 link mid-gray dim">
+              luis.aveiga@gmail.com
+            </a>
+          </div>
+        </footer>
       </div>
     );
   }
