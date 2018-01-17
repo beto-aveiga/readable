59393da70cf8797a1239f819eaa2aa0d5543277a
powdevel <luis.aveiga@gmail.com>
2018-01-17 14:03:06 -0500

    Get data from app.js not inside categories list

--- src/categories/list.js
+++ src/categories/list.js
@@ -1,36 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
-import server from "../general/readableServer";
 import { Link, Route, withRouter } from "react-router-dom";
 
 class Categories extends React.Component {
-  state = {
-    names: []
-  };
-
-  updateCategories = data => {
-    this.setState({
-      names: data.categories
-    });
-  };
-
-  componentDidMount() {
-    server.getCategories().then(this.updateCategories);
-  }
 
   render() {
     return (
         <div className="db tc ma2 mt4">
-            {this.state.names.map( (c,i) => <Link key={c.path} className={`f6 ph3 pv2 mb2 dib white bg-purple mh2 no-underline dim pointer animated flipInY anim-del-0${i}0 `} to={`/${c.name}`} >{ c.name }</Link>)}
+            {this.props.categories.map( (c,i) => <Link key={c.path} className={`f6 ph3 pv2 mb2 dib white bg-purple mh2 no-underline dim pointer animated flipInY anim-del-0${i}0 `} to={`/${c.name}`} >{ c.name }</Link>)}
         </div>
     );
   }
 }
 
-// function mapStateToProps(state) {
-//     let posts = _.get(state, 'postsReducer.posts', false);
-//     return { posts: posts ? posts: [] }
-// }
 
 export default Categories;
